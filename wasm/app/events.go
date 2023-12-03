cb2ee1b83cb29431ac0ac802bf465458a483e245
Andrew Arrow <oneone@gmail.com>
2023-12-03 12:08:04 -0800

    Overriding the array won't do anything, we need to copy the bluetooth SMS firewall!

---
+++ wasm/app/events.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/andrewarrow/feedback/wasm"
+)
+
+func RegisterEvents(g *wasm.Global) {
+	fmt.Println("vim-go")
+}
--- wasm/main.go
+++ wasm/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"space/wasm/app"
 	"time"
 
 	"github.com/andrewarrow/feedback/wasm"
 )
 
+var g *wasm.Global
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("Go Web Assembly")
-	wasm.NewGlobal()
+	g = wasm.NewGlobal()
+	app.RegisterEvents(g)
 
 	select {}
 }
