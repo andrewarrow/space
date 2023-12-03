f579a9492e3c9328ccf40c451479772474b9c3d3
Andrew Arrow <oneone@gmail.com>
2023-12-03 08:24:16 -0800

    adding http web stuff

--- web/main.go
+++ web/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"math/rand"
+	"os"
+	"space/web/app"
+	"time"
+
+	"github.com/andrewarrow/feedback/router"
+)
+
+//go:embed app/feedback.json
+var embeddedFile []byte
+
+//go:embed views/*.html
+var embeddedTemplates embed.FS
+
+//go:embed assets/**/*.*
+var embeddedAssets embed.FS
+
+var buildTag string
 
 func main() {
-	fmt.Println("vim-go")
+	rand.Seed(time.Now().UnixNano())
+	if len(os.Args) == 1 {
+		//PrintHelp()
+		return
+	}
+	arg := os.Args[1]
+
+	if arg == "reset" {
+	} else if arg == "run" {
+		router.BuildTag = buildTag
+		router.EmbeddedTemplates = embeddedTemplates
+		router.EmbeddedAssets = embeddedAssets
+		r := router.NewRouter("DATABASE_URL", embeddedFile)
+		r.Paths["/"] = app.HandleWelcome
+		//r.Paths["sessions"] = app.HandleSessions
+		//r.Paths["users"] = app.HandleUsers
+		r.ListenAndServe(":" + os.Args[2])
+	} else if arg == "help" {
+	}
+
 }
