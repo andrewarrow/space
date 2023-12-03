7f188b4d6a1112abf55919a5eb58f56c325bff08
Andrew Arrow <oneone@gmail.com>
2023-12-03 12:18:13 -0800

    We need to back up the 1080p HTTP pixel!

--- wasm/app/events.go
+++ wasm/app/events.go
@@ -2,10 +2,16 @@ package app
 
 import (
 	"fmt"
+	"syscall/js"
 
 	"github.com/andrewarrow/feedback/wasm"
 )
 
 func RegisterEvents(g *wasm.Global) {
-	fmt.Println("vim-go")
+	g.Click("login", login)
+}
+
+func login(this js.Value, p []js.Value) any {
+	fmt.Println("login")
+	return nil
 }
