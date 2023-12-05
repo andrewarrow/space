07103ddeb3a6c6bac210b6fd6585afd9d697c821
Andrew Arrow <oneone@gmail.com>
2023-12-05 12:36:31 -0800

    Use the primary PCI protocol, then you can index the redundant sensor!

--- wasm/app/events.go
+++ wasm/app/events.go
@@ -1,7 +1,9 @@
 package app
 
 import (
+	"space/wasm/network"
 	"syscall/js"
+	"time"
 
 	"github.com/andrewarrow/feedback/wasm"
 )
@@ -20,6 +22,21 @@ func login(this js.Value, p []js.Value) any {
 
 func loginForm(this js.Value, p []js.Value) any {
 	p[0].Call("preventDefault")
-	//Global.Location.Set("href", "/space")
+	form := Global.Document.ByIdWrapped("login-form")
+	m := map[string]any{}
+	for _, input := range form.SelectAll("input") {
+		m[input.Id] = input.Value
+	}
+	go func() {
+		code := network.DoPost("/login", m)
+		if code == 200 {
+			Global.Location.Set("href", "/space")
+			return
+		}
+		flash := Global.Document.ById("flash")
+		flash.Set("innerHTML", "invalid login")
+		time.Sleep(time.Second * 3)
+		flash.Set("innerHTML", "")
+	}()
 	return false
 }
