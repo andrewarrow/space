aedbb66e8c2b6481e8c97c98c99ce69c2b45a14a
Andrew Arrow <oneone@gmail.com>
2023-12-09 11:17:30 -0800

    You can't hack the interface without quantifying the bluetooth RAM interface!

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['views/*.html',],
+  theme: {
+    extend: {
+      colors: {
+        'lime': '#8FBC8F',
+      },
+    },
+  },
+  plugins: [],
+}
