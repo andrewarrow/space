e34534d81efb3f967f2e23c113ecc1b6878f3630
Andrew Arrow <oneone@gmail.com>
2023-12-09 11:23:16 -0800

    We need to reboot the primary GB alarm!

---
+++ wasm/app/login_events.go
@@ -0,0 +1,70 @@
+package app
+
+import (
+	"space/wasm/network"
+	"syscall/js"
+	"time"
+
+	"github.com/andrewarrow/feedback/wasm"
+)
+
+func RegisterLoginEvents() {
+	Global.Click("login", login)
+	Global.Click("register", register)
+	Global.Submit("login-form", loginForm)
+	Global.Submit("register-form", registerForm)
+}
+
+func login(this js.Value, p []js.Value) any {
+	form := Document.ById("login-form")
+	wasm.RemoveClass(form, "hidden")
+	form = Document.ById("register-form")
+	wasm.AddClass(form, "hidden")
+	return nil
+}
+
+func register(this js.Value, p []js.Value) any {
+	form := Document.ById("login-form")
+	wasm.AddClass(form, "hidden")
+	form = Document.ById("register-form")
+	wasm.RemoveClass(form, "hidden")
+	return nil
+}
+
+func loginForm(this js.Value, p []js.Value) any {
+	p[0].Call("preventDefault")
+	form := Document.ByIdWrapped("login-form")
+
+	go func() {
+		code := network.DoPost("/login", form.MapOfInputs())
+		if code == 200 {
+			Global.Location.Set("href", "/space")
+			return
+		}
+		flash := Document.ById("flash")
+		flash.Set("innerHTML", "invalid login")
+		time.Sleep(time.Second * 3)
+		flash.Set("innerHTML", "")
+	}()
+
+	return false
+}
+
+func registerForm(this js.Value, p []js.Value) any {
+	p[0].Call("preventDefault")
+	form := Document.ByIdWrapped("register-form")
+
+	go func() {
+		code := network.DoPost("/register", form.MapOfInputs())
+		if code == 200 {
+			Global.Location.Set("href", "/space")
+			return
+		}
+		flash := Document.ById("flash")
+		flash.Set("innerHTML", "invalid username or password")
+		time.Sleep(time.Second * 3)
+		flash.Set("innerHTML", "")
+	}()
+
+	return false
+}
---
+++ wasm/app/register.go
@@ -0,0 +1,16 @@
+package app
+
+import "github.com/andrewarrow/feedback/wasm"
+
+var Global *wasm.Global
+var Document *wasm.Document
+
+func RegisterEvents() {
+	if Global.Start == "welcome" {
+		RegisterLoginEvents()
+	} else if Global.Start == "space" {
+		//RegisterSpaceEvents()
+	} else if Global.Start == "profile" {
+		//RegisterProfileEvents()
+	}
+}
