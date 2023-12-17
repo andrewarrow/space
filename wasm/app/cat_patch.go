d638da21d211cc39f6f55e033acfbbaf6941745c
Andrew Arrow <oneone@gmail.com>
2023-12-17 09:42:09 -0800

    Try to index the SMS hard drive, maybe it will input the back-end bandwidth!

---
+++ wasm/app/cat_patch.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"space/wasm/network"
+	"syscall/js"
+)
+
+type CatPatch struct {
+	DeviceId string
+	Cat      string
+}
+
+func NewCatPatch(did, cat string) *CatPatch {
+	d := CatPatch{}
+	d.DeviceId = did
+	d.Cat = cat[4:]
+	return &d
+}
+
+func (d *CatPatch) Click(this js.Value, params []js.Value) any {
+
+	Document.ByIdWrap("back").FireClick()
+	Document.ByIdWrap("back").FireClick()
+	go func() {
+		payload := map[string]any{"cat": d.Cat}
+		network.DoPatch("/devices/"+d.DeviceId, payload)
+	}()
+
+	return nil
+}
