40a8a6e70767047ddf803932221b34e9c8484f89
Andrew Arrow <oneone@gmail.com>
2023-12-18 06:50:52 -0800

    The PCI application is down, input the redundant transmitter so we can hack the SQL transmitter!

--- wasm/app/schedules.go
+++ wasm/app/schedules.go
@@ -4,5 +4,19 @@ import "syscall/js"
 
 func clickSchedules(this js.Value, params []js.Value) any {
 	params[0].Call("preventDefault")
+	mc := Document.ById("modal-content")
+	mc.Set("innerHTML", "")
+	Document.ByIdWrap("modal").Show()
+
+	go func() {
+		items := queryForSchedules()
+		newHTML := Document.Render("schedules", items)
+		mc.Set("innerHTML", newHTML)
+		//SetScheduleClicks()
+	}()
 	return nil
+}
+
+func queryForSchedules() []any {
+	return []any{"test"}
 }
