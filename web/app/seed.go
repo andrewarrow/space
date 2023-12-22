4b8aa23622f9be9e10e556f17eac289f9bb4f82d
Andrew Arrow <oneone@gmail.com>
2023-12-22 03:29:58 -0800

    Use the back-end AGP interface, then you can compress the back-end monitor!

--- web/app/seed.go
+++ web/app/seed.go
@@ -6,6 +6,7 @@ func Seed(c *router.Context) {
 	id := makeDevice(c, "Living Room TV")
 	id = makeDevie(c, "Bedroom TV")
 	id = makeDevie(c, "Pet Feeder")
+	makeFunction(c, id, "release_food(bay,grams)")
 }
 
 func makeDevice(c *router.Context, s string) any {
@@ -17,4 +18,14 @@ func makeDevice(c *router.Context, s string) any {
 	guid := c.Params["guid"]
 	one = c.One("device", "where guid=$1", guid)
 	return one["id"]
+}
+func makeFunction(c *router.Context, id any, s string) any {
+	c.Params = map[string]any{}
+
+	c.Params["function"] = s
+	c.Params["device_id"] = id
+	c.ValidateCreate("function")
+	c.Insert("function")
+	guid := c.Params["guid"]
+	return guid
 }
