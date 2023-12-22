99b6b86e654d1f5cdcc019498174e5577f548b5f
Andrew Arrow <oneone@gmail.com>
2023-12-22 03:33:03 -0800

    I'Ll calculate the bluetooth XSS interface, that should generate the PCI capacitor!

--- web/app/seed.go
+++ web/app/seed.go
@@ -4,9 +4,10 @@ import "github.com/andrewarrow/feedback/router"
 
 func Seed(c *router.Context) {
 	id := makeDevice(c, "Living Room TV")
-	id = makeDevie(c, "Bedroom TV")
-	id = makeDevie(c, "Pet Feeder")
+	id = makeDevice(c, "Bedroom TV")
+	id = makeDevice(c, "Pet Feeder")
 	makeFunction(c, id, "release_food(bay,grams)")
+	makePayload(c, id, PET_JSON)
 }
 
 func makeDevice(c *router.Context, s string) any {
@@ -16,7 +17,7 @@ func makeDevice(c *router.Context, s string) any {
 	c.ValidateCreate("device")
 	c.Insert("device")
 	guid := c.Params["guid"]
-	one = c.One("device", "where guid=$1", guid)
+	one := c.One("device", "where guid=$1", guid)
 	return one["id"]
 }
 func makeFunction(c *router.Context, id any, s string) any {
@@ -29,3 +30,15 @@ func makeFunction(c *router.Context, id any, s string) any {
 	guid := c.Params["guid"]
 	return guid
 }
+func makePayload(c *router.Context, id any, s string) any {
+	c.Params = map[string]any{}
+
+	c.Params["payload"] = s
+	c.Params["device_id"] = id
+	c.ValidateCreate("payload")
+	c.Insert("payload")
+	guid := c.Params["guid"]
+	return guid
+}
+
+const PET_JSON = `{"bays": [ {"id": "BAY_01", "sku": "MEOW_MIX_1010", "flavor": "Chicken and Yumminess", "grams": 2000}, {"id": "BAY_02", "sku": "MEOW_MIX_1020", "flavor": "Fresh Sardines", "grams": 1900} ] }`
