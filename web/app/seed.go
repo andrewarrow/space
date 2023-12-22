9f7897fecc3e76408e6afe272e7b79efa2c05db3
Andrew Arrow <oneone@gmail.com>
2023-12-22 03:36:39 -0800

    Programming the matrix won't do anything, we need to copy the digital SAS circuit!

--- web/app/seed.go
+++ web/app/seed.go
@@ -7,7 +7,16 @@ func Seed(c *router.Context) {
 	id = makeDevice(c, "Bedroom TV")
 	id = makeDevice(c, "Pet Feeder")
 	makeFunction(c, id, "release_food(bay,grams)")
+	makeFunction(c, id, "play_audio(file)")
+	makeFunction(c, id, "take_photo")
 	makePayload(c, id, PET_JSON)
+
+	id = makeDevice(c, "Fridge")
+	makeFunction(c, id, "set_temperature(celsius)")
+	makeFunction(c, id, "set_temperature(fahrenheit)")
+	makeFunction(c, id, "play_audio(file)")
+	makeFunction(c, id, "take_photo")
+	makePayload(c, id, FRIDGE)
 }
 
 func makeDevice(c *router.Context, s string) any {
@@ -41,4 +50,8 @@ func makePayload(c *router.Context, id any, s string) any {
 	return guid
 }
 
+const FRIDGE = `{ "fridgeId": "SFR123456", "model": "SmartFreeze 9000", "temperature": 2.5, "humidity": 45, "status": "operational", "lastUpdate": "2023-12-16T08:30:00Z", "inventory": [ { "item": "Milk", "quantity": 2, "expiryDate": "2023-12-20" }, { "item": "Eggs", "quantity": 1, "expiryDate": "2023-12-18" }, { "item": "Yogurt", "quantity": 3, "expiryDate": "2023-12-22" }, { "item": "Cheese", "quantity": 1, "expiryDate": "2023-12-25" }, { "item": "Vegetables", "quantity": 5, "expiryDate": null } ], "energyConsumption": { "current": 120, "average": 100, "unit": "Watt-hours" }, "alerts": [ { "type": "temperature", "message": "Temperature is higher than usual. Check settings." }, { "type": "inventory", "message": "Low quantity of eggs. Consider restocking." } ], "settings": { "temperatureUnit": "Celsius", "humidityAlertThreshold": 50 } }`
+
 const PET_JSON = `{"bays": [ {"id": "BAY_01", "sku": "MEOW_MIX_1010", "flavor": "Chicken and Yumminess", "grams": 2000}, {"id": "BAY_02", "sku": "MEOW_MIX_1020", "flavor": "Fresh Sardines", "grams": 1900} ] }`
+
+const IPHONE = `{ "device_info": { "device_name": "Andrew iPhone", "model": "iPhone SE", "os_version": "iOS 15.3", "serial_number": "1234567890", "battery": { "battery_percentage": 75, "battery_status": "Charging", "charging_source": "Lightning Cable" }, "storage": { "total_storage": "128 GB", "available_storage": "64 GB", "used_storage": "64 GB" } }, "system_status": { "cpu_usage": "25%", "memory": { "total_memory": "4 GB", "used_memory": "2 GB", "free_memory": "2 GB" }, "network": { "wifi": { "status": "Connected", "signal_strength": "Excellent" }, "cellular": { "status": "4G", "signal_strength": "High" } } }, "apps": { "running_apps": [ { "name": "Safari", "status": "Active", "cpu_usage": "10%", "memory_usage": "150 MB" }, { "name": "Messages", "status": "Inactive", "cpu_usage": "5%", "memory_usage": "80 MB" }, { "name": "Music", "status": "Active", "cpu_usage": "8%", "memory_usage": "120 MB" } ] }, "security": { "face_id": { "enabled": false }, "touch_id": { "enabled": true }, "passcode": { "enabled": true, "complexity": "Medium" } } }`
